72600515e830382f76c7c77206b22a46e1de1764
JaeHo Song <thruthesky@gmail.com>
2023-06-10 21:33:51 +0800

    adding user create

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -24,9 +25,52 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  build(_) => const Text("Hello");
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final email = TextEditingController();
+  final password = TextEditingController();
+  @override
+  build(context) => Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Core Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            TextField(controller: email),
+            const SizedBox(height: 16),
+            TextField(controller: password),
+            const SizedBox(height: 16),
+            ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final credential = await FirebaseAuth.instance
+                        .createUserWithEmailAndPassword(
+                      email: email.text,
+                      password: password.text,
+                    );
+                    print(credential.user);
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'weak-password') {
+                      print('The password provided is too weak.');
+                    } else if (e.code == 'email-already-in-use') {
+                      print('The account already exists for that email.');
+                    } else {
+                      print(e);
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+                child: const Text('Register')),
+          ],
+        ),
+      ));
 }
